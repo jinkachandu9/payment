40f005744e3e696492b3e498ddd6b987ea130d28
jinkachandu9 <jinkachandu9@gmail.com>
2021-01-12 01:24:09 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -84,7 +84,7 @@ function App() {
       },
       body: JSON.stringify(data)
     };
-    fetch('https://assignment-roiim-chandu.herokuapp.com/api/token', requestOptions)
+    fetch('https://roiim1.herokuapp.com/api/token', requestOptions)
       .then(response => response.json())
       .then(result => {
         // console.log(result);
